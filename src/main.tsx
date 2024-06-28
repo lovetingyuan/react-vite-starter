72eaa6167073ea08a023f8d21700aa9c5ec82490
tingyuan <1932294867@qq.com>
2024-06-29 01:36:21 +0800

    -

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const useStrict = !location.search.includes('strict=off')
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  useStrict ? (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ) : (
     <App />
-  </React.StrictMode>
+  )
 )
